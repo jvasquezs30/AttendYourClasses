5ce3eebb31bb50b2a9740f69b7b1f71bf0f07f39
OT-0043 <otapiass@unicartagena.edu.co>
2026-05-26 16:03:49 -0500

    Update Curso.java

--- src/main/java/modelo/academico/Curso.java
+++ src/main/java/modelo/academico/Curso.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package modelo.academico;
+package modelo.academico;//
 
 import modelo.usuarios.Docente;
 import modelo.usuarios.Estudiante;
