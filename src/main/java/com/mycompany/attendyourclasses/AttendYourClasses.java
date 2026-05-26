eccbe44fca78e131568c5ee65264e8f480470aab
jvasquezs30 <jvasquezs@unicartagena.edu.co>
2026-05-26 14:41:09 -0500

    Creacion de los Paquetes y clases
    Se hizo la creacion de los paquetes y clases, se dividio los modelos en varios partes para mejor entendimiento, tambien incluye su vista y controlador, anexe un paquete main para incluir ahi el main del programa

--- src/main/java/com/mycompany/attendyourclasses/AttendYourClasses.java
+++ src/main/java/com/mycompany/attendyourclasses/AttendYourClasses.java
@@ -11,6 +11,6 @@ package com.mycompany.attendyourclasses;
 public class AttendYourClasses {
 
     public static void main(String[] args) {
-        System.out.println("Prueba!");
+        System.out.println("Pruebajjj!");
     }
 }
